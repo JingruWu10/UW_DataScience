b2868ff3d483bbe543935b3d3c1f733ebbdbbcba
Unknown <leo.salemann@me.com>
2018-05-14 12:14:14 -0700

    End of class commit

--- Q3/HW04/model_selection.R
+++ Q3/HW04/model_selection.R
@@ -1,17 +1,17 @@
-file <- "C:\\UW\\data\\Bank Data.csv"
+file <-"/home/vagrant/git/UW_DataScience/Q3/HW03/Bank Data.csv"
 data <- read.csv(file, sep=",", header=TRUE)
 
 summary(data)
 
-glm.full <- glm(pep ~ ., data = data, family = binomial)
-glm.null <- glm(pep ~ 1, data = data, family = binomial)
+glm.full <- glm(pep ~ ., data = data, family = binomial)  #use all feaures
+glm.null <- glm(pep ~ 1, data = data, family = binomial)  #use one features
 
 library(rms)
 lrm.full <- rms::lrm(pep ~ ., data = data)
 fastbw(lrm.full, rule = "p", sls = 0.1)
 
 model.aic.backward <- step(glm.full, direction = "backward", trace = 1)
-summary(model.aic.backward)
+summary(model.aic.backward) #minum AIC is best
 
 model.aic.forward <- step(glm.null, direction = "forward", trace = 1, 
                           scope = ~ age + sex + region + income + married + children + car + save_act +
@@ -19,7 +19,7 @@ model.aic.forward <- step(glm.null, direction = "forward", trace = 1,
 
 summary(model.aic.forward)
 
-model.aic.both <- step(lm.null, direction = "both", trace = 1, 
+model.aic.both <- step(glm.null, direction = "both", trace = 1, 
                        scope = ~ age + sex + region + income + married + children + car + save_act +
                          current_act + mortgage)
 summary(model.aic.both)
